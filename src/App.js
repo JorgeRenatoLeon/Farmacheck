b2ac767c9ef5fbf354eaf83b814cfbdbeb7c7fd8
Jorge <student5@jala-foundation.org>
2021-10-09 12:22:15 -0500

    Index Page Structure

--- src/App.js
+++ src/App.js
@@ -4,24 +4,40 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="top-logos">
-        <div className="top-logo-icon">
-          <img 
-            src="https://www.pngfind.com/pngs/m/126-1265657_spotifylogoswap-walmart-logo-sin-fondo-hd-png-download.png"
-            alt="logo" width="50px" height="50px"/>
-        </div>
-        <div className="top-logo-icon">
-          <img 
-            src="https://www.pngfind.com/pngs/m/126-1265657_spotifylogoswap-walmart-logo-sin-fondo-hd-png-download.png"
-            alt="logo" width="50px" height="50px"/>
-        </div>
-      </div>
       <div className="logo-container">
         <div className="logo">
           <img src="https://www.pngfind.com/pngs/m/126-1265657_spotifylogoswap-walmart-logo-sin-fondo-hd-png-download.png" alt="logo" width="200px" height="200px"/>
         </div>
-        <div className="logo-text">
-          Farma
+        <div className="text-container">
+          <div className="logo-text">
+            Farma
+          </div>
+          <div className="logo-icon">
+            <img src="https://www.pngfind.com/pngs/m/126-1265657_spotifylogoswap-walmart-logo-sin-fondo-hd-png-download.png" alt="logo" width="50px" height="50px"/>
+          </div>
+        </div>
+        <div className="text-description-container">
+          <div className="text-description">
+            Tu decides dónde compras tus medicinas al mejor precio
+          </div>
+        </div>
+        <div class="search-container input-group mb-3">
+          <span class="input-group-text" id="basic-addon1">
+            <i class="bi-alarm" style={{fontSize: "2rem", color: "cornflowerblue"}}></i>
+          </span>
+          <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1"/>
+        </div>
+      </div>
+      <div className="bottom-logos">
+        <div className="bottom-logo-icon">
+          <img 
+            src="https://www.pngfind.com/pngs/m/126-1265657_spotifylogoswap-walmart-logo-sin-fondo-hd-png-download.png"
+            alt="logo" width="50px" height="50px"/>
+        </div>
+        <div className="bottom-logo-icon">
+          <img 
+            src="https://www.pngfind.com/pngs/m/126-1265657_spotifylogoswap-walmart-logo-sin-fondo-hd-png-download.png"
+            alt="logo" width="50px" height="50px"/>
         </div>
       </div>
     </div>
