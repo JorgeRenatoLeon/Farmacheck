24a19557c51156911e8bda9d4a7c6838f7ed75bc
Andrea <andrea.reyesb@pucp.edu.pe>
2021-10-17 19:03:25 -0500

    icon to T&C

--- src/components/CommentsCard/CommentsCard.js
+++ src/components/CommentsCard/CommentsCard.js
@@ -1,11 +1,16 @@
 import React from "react";
 import './commentsCard.scss';
+import { Link } from "react-router-dom";
+import * as ROUTES from "../../routes/routes";
 
 const CommentsCard = (props) => {
 
   return (      
     <div className="card-comments">         
-        <p className="first-text">Envianos tus comentarios a</p> 
+        <p className="first-text">
+          <Link to={ROUTES.TERMSANDCONDITIONS} ><div className="icon-style"><i className="bi bi-exclamation-circle-fill"></i></div></Link>
+          Envianos tus comentarios a
+        </p> 
         <p className="second-text">info@farmacheck.pe</p>       
     </div>  
   );
